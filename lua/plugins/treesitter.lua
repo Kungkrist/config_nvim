5827e9f46cf89f6da5b25ae3e08a13b4ba86f731
kungkrist <hehe>
2026-03-13 20:38:15 +0100

    Force treesitter to use master branch due do depricated dependencies

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,6 +2,8 @@ return {
 	{
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
+		lazy = false,
+		branch = "master",
 		config = function()
 			require 'nvim-treesitter.configs'.setup {
 				ensure_installed = {
