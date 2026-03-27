0feaf0f2463086a84ea76eb53948bb0db25fc45a
Kungkrist <andhan0305@gmail.com>
2026-03-28 00:30:07 +0100

    Add color picker and color highlighter plugin

--- lua/plugins/one_liners.lua
+++ lua/plugins/one_liners.lua
@@ -1,5 +1,14 @@
 return {
 	{
+		"uga-rosa/ccc.nvim",
+		opts = {
+			highlighter = {
+				auto_enable = true,
+				lsp = true,
+			},
+		},
+	},
+	{
 		"folke/zen-mode.nvim",
 		opts = {
 			-- your configuration comes here
@@ -12,20 +21,20 @@ return {
 		lazy = false,
 		priority = 1000,
 		opts = {
-			transparent = true, -- Disable background color
-			terminal_colors = true, -- Configure terminal colors
+			-- transparent = true, -- Disable background color
+			-- terminal_colors = true, -- Configure terminal colors
 		},
 		styles = {
 			comments = { italic = true }, -- Style for comments
 			keywords = { italic = true }, -- Style for keywords
-			functions = {},       -- Style for functions
-			variables = {},       -- Style for variables
-			sidebars = "dark",    -- Style for sidebars
-			floats = "dark",      -- Style for floating windows
+			functions = {},      -- Style for functions
+			variables = {},      -- Style for variables
+			sidebars = "dark",   -- Style for sidebars
+			floats = "dark",     -- Style for floating windows
 		},
 		config = function()
 			vim.cmd.colorscheme("solarized-osaka")
-			-- vim.cmd("hi Normal guibg=#001116 ctermbg=NONE")
+			vim.cmd("hi Normal guibg=#001116 ctermbg=NONE")
 			-- vim.cmd.colorscheme("catppuccin")
 			-- vim.cmd [[
 			--   hi Normal guibg=NONE ctermbg=NONE
