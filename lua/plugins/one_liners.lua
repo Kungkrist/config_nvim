7e7e5a4a6998445e4f1d3b0f36fc2ffb3cd63995
kungkrist <hehe>
2026-03-27 23:02:37 +0100

    Change theme

--- lua/plugins/one_liners.lua
+++ lua/plugins/one_liners.lua
@@ -1,25 +1,73 @@
 return {
 	{
-		-- Color theme
-		"folke/tokyonight.nvim",
+		"folke/zen-mode.nvim",
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		}
+	},
+	{
+		"craftzdog/solarized-osaka.nvim",
+		lazy = false,
 		priority = 1000,
 		opts = {
-			transparent = true,
-			styles = {
-				sidebars = "transparent",
-				floats = "transparent",
-			},
+			transparent = true, -- Disable background color
+			terminal_colors = true, -- Configure terminal colors
+		},
+		styles = {
+			comments = { italic = true }, -- Style for comments
+			keywords = { italic = true }, -- Style for keywords
+			functions = {},       -- Style for functions
+			variables = {},       -- Style for variables
+			sidebars = "dark",    -- Style for sidebars
+			floats = "dark",      -- Style for floating windows
 		},
 		config = function()
-			vim.cmd.colorscheme("tokyonight-moon")
-			vim.cmd [[
-			  hi Normal guibg=NONE ctermbg=NONE
-			  hi NormalNC guibg=NONE ctermbg=NONE
-			  hi NormalFloat guibg=NONE ctermbg=NONE
-			  hi SignColumn guibg=NONE ctermbg=NONE
-			]]
+			vim.cmd.colorscheme("solarized-osaka")
+			-- vim.cmd("hi Normal guibg=#001116 ctermbg=NONE")
+			-- vim.cmd.colorscheme("catppuccin")
+			-- vim.cmd [[
+			--   hi Normal guibg=NONE ctermbg=NONE
+			--   hi NormalNC guibg=NONE ctermbg=NONE
+			--   hi NormalFloat guibg=NONE ctermbg=NONE
+			--   hi SignColumn guibg=NONE ctermbg=NONE
+			-- ]]
 		end
 	},
+	-- {
+	-- 	-- Color theme
+	-- 	"folke/tokyonight.nvim",
+	-- 	-- "catppuccin/nvim",
+	-- 	priority = 1000,
+	-- 	opts = {
+	-- 		transparent = false,
+	-- 		styles = {
+	-- 			sidebars = "transparent",
+	-- 			floats = "transparent",
+	-- 		},
+	-- 	},
+	-- 	config = function()
+	-- 		vim.cmd.colorscheme("tokyonight-moon")
+	-- 		-- vim.cmd.colorscheme("catppuccin")
+	-- 		-- vim.cmd [[
+	-- 		--   hi Normal guibg=NONE ctermbg=NONE
+	-- 		--   hi NormalNC guibg=NONE ctermbg=NONE
+	-- 		--   hi NormalFloat guibg=NONE ctermbg=NONE
+	-- 		--   hi SignColumn guibg=NONE ctermbg=NONE
+	-- 		-- ]]
+	-- 	end
+	-- },
+	-- {
+	-- 	"tiagovla/tokyodark.nvim",
+	-- 	opts = {
+	-- 		-- custom options here
+	-- 	},
+	-- 	config = function(_, opts)
+	-- 		require("tokyodark").setup(opts) -- calling setup is optional
+	-- 		vim.cmd [[colorscheme tokyodark]]
+	-- 	end,
+	-- },
 	{
 		'nvim-lualine/lualine.nvim',
 		dependencies = { 'nvim-tree/nvim-web-devicons' },
