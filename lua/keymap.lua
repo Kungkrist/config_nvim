13d8e2ac155b72aec093ffd20ba803a4db7cf2f4
kungkrist <hehe>
2026-04-03 19:40:11 +0200

    Add some telekasten keybinds and fix conflict

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -6,7 +6,7 @@ vim.keymap.set("n", "<leader>bd", ":bdelete<CR>", { desc = "Buffer: Delete (bdel
 
 -- lsp
 vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format { async = true } end,
-	{ desc = "LSP: Format" })
+  { desc = "LSP: Format" })
 vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "LSP: Definition" })
 vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "LSP: References" })
 vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "LSP: Implementation" })
@@ -14,22 +14,22 @@ vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "LSP: Hover" })
 vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "LSP: Rename" })
 vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP: CodeAction" })
 vim.keymap.set("n", "gl", function()
-	-- Get diagnostics for the current line
-	local line_diagnostics = vim.diagnostic.get(0, { lnum = vim.fn.line('.') - 1 })
+  -- Get diagnostics for the current line
+  local line_diagnostics = vim.diagnostic.get(0, { lnum = vim.fn.line('.') - 1 })
 
-	-- Filter for warnings & errors only
-	local filtered = {}
-	for _, d in ipairs(line_diagnostics) do
-		if d.severity == vim.diagnostic.severity.WARN
-			or d.severity == vim.diagnostic.severity.ERROR then
-			table.insert(filtered, d)
-		end
-	end
+  -- Filter for warnings & errors only
+  local filtered = {}
+  for _, d in ipairs(line_diagnostics) do
+    if d.severity == vim.diagnostic.severity.WARN
+        or d.severity == vim.diagnostic.severity.ERROR then
+      table.insert(filtered, d)
+    end
+  end
 
-	-- Show in a floating window
-	if #filtered > 0 then
-		vim.diagnostic.open_float({ scope = "line", severity = { min = vim.diagnostic.severity.WARN } })
-	end
+  -- Show in a floating window
+  if #filtered > 0 then
+    vim.diagnostic.open_float({ scope = "line", severity = { min = vim.diagnostic.severity.WARN } })
+  end
 end, { desc = "LSP: Show warnings/errors on this line" })
 
 -- Outline
@@ -38,17 +38,17 @@ vim.keymap.set("n", "<leader>at", ":Outline<CR>", { desc = "Outline: Toggle" })
 -- grug-far (search and replace)
 local grug_far = require("grug-far")
 vim.keymap.set("n", "SAC", function()
-	grug_far.open({
-		-- engine = 'astgrep',
-		transient = true,
-		prefills = { search = vim.fn.expand("<cword>") },
-	})
+  grug_far.open({
+    -- engine = 'astgrep',
+    transient = true,
+    prefills = { search = vim.fn.expand("<cword>") },
+  })
 end, { desc = "Find And Replace: Cursor All files" })
 vim.keymap.set("n", "SAF", function()
-	grug_far.open({
-		-- engine = 'ashtgrep',
-		transient = true,
-	})
+  grug_far.open({
+    -- engine = 'ashtgrep',
+    transient = true,
+  })
 end, { desc = "Find And Replace: All files" })
 
 --- yazi
@@ -81,63 +81,63 @@ vim.keymap.set('n', '<leader>tgf', builtin.git_files, { desc = "Telescope: find 
 vim.keymap.set('n', '<leader>tkm', builtin.keymaps, { desc = "Telescope: keymaps" })
 vim.keymap.set('n', '<leader>tjl', builtin.jumplist, { desc = "Telescope: jumplist" })
 vim.keymap.set('n', '<leader>tfaf', function()
-	builtin.find_files({
-		hidden = true, -- include hidden files
-		no_ignore = true, -- include gitignored files
-		follow = true, -- follow symlinks
-	})
+  builtin.find_files({
+    hidden = true,  -- include hidden files
+    no_ignore = true, -- include gitignored files
+    follow = true,  -- follow symlinks
+  })
 end, { desc = "Telescope: find files including hidden/gitignored" })
 
 -- telescope / grep
 vim.keymap.set('n', '<leader>tlg', builtin.jumplist, { desc = "Telescope: live grep" })
 vim.keymap.set('n', '<leader>tgs', function()
-	builtin.grep_string({ search = vim.fn.input("grep > ") })
+  builtin.grep_string({ search = vim.fn.input("grep > ") })
 end, { desc = "Telescope: grep string input" })
 
 -- telescope / git
 vim.keymap.set('n', '<leader>gs', builtin.git_stash, { desc = "Telescope: Git Stash" })
 vim.keymap.set('n', '<leader>gbb', builtin.git_branches, { desc = "Telescope: Branches" })
 vim.keymap.set("n", "<leader>gc", function()
-	builtin.git_commits({
-		attach_mappings = function(_, map)
-			map("i", "<CR>", function(prompt_bufnr)
-				local entry = action_state.get_selected_entry()
-				actions.close(prompt_bufnr)
-				vim.cmd("Gvdiffsplit " .. entry.value)
-			end)
-			map("n", "<CR>", function(prompt_bufnr)
-				local entry = action_state.get_selected_entry()
-				actions.close(prompt_bufnr)
-				vim.cmd("Gvdiffsplit " .. entry.value)
-			end)
-			return true
-		end,
-	})
+  builtin.git_commits({
+    attach_mappings = function(_, map)
+      map("i", "<CR>", function(prompt_bufnr)
+        local entry = action_state.get_selected_entry()
+        actions.close(prompt_bufnr)
+        vim.cmd("Gvdiffsplit " .. entry.value)
+      end)
+      map("n", "<CR>", function(prompt_bufnr)
+        local entry = action_state.get_selected_entry()
+        actions.close(prompt_bufnr)
+        vim.cmd("Gvdiffsplit " .. entry.value)
+      end)
+      return true
+    end,
+  })
 end, { desc = "Telescope: Git Commits with Diff" })
 
 vim.keymap.set("n", "<leader>gbc", function()
-	builtin.git_bcommits({
-		attach_mappings = function(_, map)
-			map("i", "<CR>", function(prompt_bufnr)
-				local entry = action_state.get_selected_entry()
-				actions.close(prompt_bufnr)
-				vim.cmd("Gvdiffsplit " .. entry.value)
-			end)
-			map("n", "<CR>", function(prompt_bufnr)
-				local entry = action_state.get_selected_entry()
-				actions.close(prompt_bufnr)
-				vim.cmd("Gvdiffsplit " .. entry.value)
-			end)
-			return true
-		end,
-	})
+  builtin.git_bcommits({
+    attach_mappings = function(_, map)
+      map("i", "<CR>", function(prompt_bufnr)
+        local entry = action_state.get_selected_entry()
+        actions.close(prompt_bufnr)
+        vim.cmd("Gvdiffsplit " .. entry.value)
+      end)
+      map("n", "<CR>", function(prompt_bufnr)
+        local entry = action_state.get_selected_entry()
+        actions.close(prompt_bufnr)
+        vim.cmd("Gvdiffsplit " .. entry.value)
+      end)
+      return true
+    end,
+  })
 end, { desc = "Telescope: Buffer Git Commits with Diff" })
 
 -- lsp
 vim.keymap.set("n", "<leader>lih", function()
-	local bufnr = vim.api.nvim_get_current_buf()
-	local enabled = vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr })
-	vim.lsp.inlay_hint.enable(not enabled, { bufnr = bufnr })
+  local bufnr = vim.api.nvim_get_current_buf()
+  local enabled = vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr })
+  vim.lsp.inlay_hint.enable(not enabled, { bufnr = bufnr })
 end, { desc = "LSP: toggle lsp inlay hints" })
 
 -- harpoon
@@ -148,9 +148,9 @@ vim.keymap.set('n', '<leader>hd', harpoon_mark.rm_file, { desc = 'Harpoon: Delet
 vim.keymap.set('n', '<leader>hn', harpoon_ui.nav_next, { desc = 'Harpoon: Nav Next' })
 vim.keymap.set('n', '<leader>hp', harpoon_ui.nav_prev, { desc = 'Harpoon: Nav Pref' })
 for i = 1, 9 do
-	vim.keymap.set("n", "<leader>h" .. i, function()
-		harpoon_ui.nav_file(i)
-	end, { desc = "Harpoon jump to mark " .. i })
+  vim.keymap.set("n", "<leader>h" .. i, function()
+    harpoon_ui.nav_file(i)
+  end, { desc = "Harpoon jump to mark " .. i })
 end
 
 -- dap
@@ -167,7 +167,7 @@ vim.keymap.set('n', '<f11>', dap.step_into, { desc = 'Dap: Step Into' })
 vim.keymap.set('n', '<f12>', dap.step_out, { desc = 'Dap: Step Out' })
 vim.keymap.set('n', '<F9>', dap.toggle_breakpoint, { desc = 'Dap: Toggle Breakpoint' })
 vim.keymap.set('n', '<leader>drl', function()
-	dap.run_last()
+  dap.run_last()
 end, { desc = 'run last debug session' })
 
 -- dapui
@@ -184,12 +184,16 @@ vim.keymap.set('t', '<A-Esc>', '<C-\\><C-n>', { desc = 'Terminal: Exit terminal 
 vim.keymap.set("n", "<leader>z", ":ZenMode<CR>", { desc = "ZenMode: Toggle" })
 
 --  Telekaster (note manager)
-vim.keymap.set("n", "gd", function()
-	local ft = vim.bo.filetype
-
-	if ft == "markdown" then
-		require("telekasten").follow_link()
-	else
-		vim.lsp.buf.definition()
-	end
-end)
+local telekasten = require("telekasten")
+vim.keymap.set("n", "<leader>ni", function()
+  telekasten.insert_link()
+end, { desc = "Telekaster: Follow link (Goto destination)" })
+vim.keymap.set("n", "<leader>ngd", function()
+  telekasten.follow_link()
+end, { desc = "Telekaster: Follow link (Goto destination)" })
+vim.keymap.set("n", "<leader>nf", function()
+  telekasten.search_notes()
+end, { desc = "Telekaster: Search in notes" })
+vim.keymap.set("n", "<leader>nrn", function()
+  telekasten.rename_note()
+end, { desc = "Telekaster: Rename note" })
--- lua/plugins/one_liners.lua
+++ lua/plugins/one_liners.lua
@@ -1,174 +1,173 @@
 return {
-	{
-		"uga-rosa/ccc.nvim",
-		opts = {
-			highlighter = {
-				auto_enable = true,
-				lsp = true,
-			},
-		},
-	},
-	{
-		"nvim-telekasten/telekasten.nvim",
-		config = function()
-			local function get_notes_dir()
-				local cwd = vim.loop.cwd()
+  {
+    "uga-rosa/ccc.nvim",
+    opts = {
+      highlighter = {
+        auto_enable = true,
+        lsp = true,
+      },
+    },
+  },
+  {
+    "nvim-telekasten/telekasten.nvim",
+    config = function()
+      local function get_notes_dir()
+        local cwd = vim.loop.cwd()
 
-				-- look for a project-specific marker
-				if vim.fn.filereadable(cwd .. "/.notes") == 1 then
-					return cwd .. "/notes"
-				end
+        -- look for a project-specific marker
+        if vim.fn.filereadable(cwd .. "/.notes") == 1 then
+          return cwd
+        end
 
-				-- fallback (global notes)
-				return vim.fn.expand("~/notes")
-			end
-
-			require("telekasten").setup({
-				home = get_notes_dir(),
-				follow_creates_nonexisting = true,
-				external_link_follow = true, -- Enable external file linking (default: true)
-			})
-		end
-	},
-	{
-		"folke/zen-mode.nvim",
-		opts = {
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
-		}
-	},
-	{
-		"craftzdog/solarized-osaka.nvim",
-		lazy = false,
-		priority = 1000,
-		opts = {
-			-- transparent = true, -- Disable background color
-			-- terminal_colors = true, -- Configure terminal colors
-		},
-		styles = {
-			comments = { italic = true }, -- Style for comments
-			keywords = { italic = true }, -- Style for keywords
-			functions = {},      -- Style for functions
-			variables = {},      -- Style for variables
-			sidebars = "dark",   -- Style for sidebars
-			floats = "dark",     -- Style for floating windows
-		},
-		config = function()
-			vim.cmd.colorscheme("solarized-osaka")
-			vim.cmd("hi Normal guibg=#001116 ctermbg=NONE")
-			-- vim.cmd.colorscheme("catppuccin")
-			-- vim.cmd [[
-			--   hi Normal guibg=NONE ctermbg=NONE
-			--   hi NormalNC guibg=NONE ctermbg=NONE
-			--   hi NormalFloat guibg=NONE ctermbg=NONE
-			--   hi SignColumn guibg=NONE ctermbg=NONE
-			-- ]]
-		end
-	},
-	-- {
-	-- 	-- Color theme
-	-- 	"folke/tokyonight.nvim",
-	-- 	-- "catppuccin/nvim",
-	-- 	priority = 1000,
-	-- 	opts = {
-	-- 		transparent = false,
-	-- 		styles = {
-	-- 			sidebars = "transparent",
-	-- 			floats = "transparent",
-	-- 		},
-	-- 	},
-	-- 	config = function()
-	-- 		vim.cmd.colorscheme("tokyonight-moon")
-	-- 		-- vim.cmd.colorscheme("catppuccin")
-	-- 		-- vim.cmd [[
-	-- 		--   hi Normal guibg=NONE ctermbg=NONE
-	-- 		--   hi NormalNC guibg=NONE ctermbg=NONE
-	-- 		--   hi NormalFloat guibg=NONE ctermbg=NONE
-	-- 		--   hi SignColumn guibg=NONE ctermbg=NONE
-	-- 		-- ]]
-	-- 	end
-	-- },
-	-- {
-	-- 	"tiagovla/tokyodark.nvim",
-	-- 	opts = {
-	-- 		-- custom options here
-	-- 	},
-	-- 	config = function(_, opts)
-	-- 		require("tokyodark").setup(opts) -- calling setup is optional
-	-- 		vim.cmd [[colorscheme tokyodark]]
-	-- 	end,
-	-- },
-	{
-		'nvim-lualine/lualine.nvim',
-		dependencies = { 'nvim-tree/nvim-web-devicons' },
-		config = function()
-			require('lualine').setup()
-		end
-	},
-	{
-		"folke/todo-comments.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		opts = {}
-	},
-	{ "nvim-lua/plenary.nvim" },
-	{
-		-- Fuzzy finder for files, buffers, and more
-		"nvim-telescope/telescope.nvim",
-		dependencies = { "plenary.nvim" }
-	},
-	{
-		-- Closes some gaps that exist between mason.nvim and nvim-dap
-		"jay-babu/mason-nvim-dap.nvim",
-		dependencies = { "williamboman/mason.nvim", "mfussenegger/nvim-dap" }
-	},
-	{ 'tpope/vim-fugitive' }, -- Git plugin
-	{ "mbbill/undotree" }, -- Inspect undo history of files
-	{ "BurntSushi/ripgrep" }, -- Used by telescope
-	{
-		'MeanderingProgrammer/render-markdown.nvim',
-		dependencies = { 'nvim-treesitter/nvim-treesitter' },
-		opts = {},
-	},
-	{
-		'MagicDuck/grug-far.nvim',
-		-- Note (lazy loading): grug-far.lua defers all it's requires so it's lazy by default
-		-- additional lazy config to defer loading is not really needed...
-		config = function()
-			-- optional setup call to override plugin options
-			-- alternatively you can set options with vim.g.grug_far = { ... }
-			require('grug-far').setup({
-				-- options, see Configuration section below
-				-- there are no required options atm
-			});
-		end
-	},
-	{
-		"hedyhli/outline.nvim",
-		lazy = true,
-		cmd = { "Outline", "OutlineOpen" },
-		opts = {
-			outline_window = {
-				-- open as right sidebar
-				focus_on_open = false,
-				split_command = "rightbelow vsplit",
-				show_numbers = true, -- real line numbers
-				show_relative_numbers = true,
-				width = 25,
-			},
-			outline_items = {
-				show_symbol_details = true,
-				show_symbol_lineno = true, -- show lineno in the outline
-				flatten_hierachy = true,
-			},
-			auto_preview = false, -- optional: prevents jumping while browsing
-			symbol_folding = {
-				autofold_depth = false,
-			},
-			preview_window = {
-				auto_preview = true,
-				open_hover_on_preview = true,
-			}
-		},
-	}
+        -- fallback (global notes)
+        return vim.fn.expand("~/notes")
+      end
+      require("telekasten").setup({
+        home = get_notes_dir(),
+        take_over_my_home = false,
+        auto_set_filetype = false,
+      })
+    end
+  },
+  {
+    "folke/zen-mode.nvim",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  },
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      -- transparent = true, -- Disable background color
+      -- terminal_colors = true, -- Configure terminal colors
+    },
+    styles = {
+      comments = { italic = true }, -- Style for comments
+      keywords = { italic = true }, -- Style for keywords
+      functions = {},               -- Style for functions
+      variables = {},               -- Style for variables
+      sidebars = "dark",            -- Style for sidebars
+      floats = "dark",              -- Style for floating windows
+    },
+    config = function()
+      vim.cmd.colorscheme("solarized-osaka")
+      vim.cmd("hi Normal guibg=#001116 ctermbg=NONE")
+      -- vim.cmd.colorscheme("catppuccin")
+      -- vim.cmd [[
+      --   hi Normal guibg=NONE ctermbg=NONE
+      --   hi NormalNC guibg=NONE ctermbg=NONE
+      --   hi NormalFloat guibg=NONE ctermbg=NONE
+      --   hi SignColumn guibg=NONE ctermbg=NONE
+      -- ]]
+    end
+  },
+  -- {
+  -- 	-- Color theme
+  -- 	"folke/tokyonight.nvim",
+  -- 	-- "catppuccin/nvim",
+  -- 	priority = 1000,
+  -- 	opts = {
+  -- 		transparent = false,
+  -- 		styles = {
+  -- 			sidebars = "transparent",
+  -- 			floats = "transparent",
+  -- 		},
+  -- 	},
+  -- 	config = function()
+  -- 		vim.cmd.colorscheme("tokyonight-moon")
+  -- 		-- vim.cmd.colorscheme("catppuccin")
+  -- 		-- vim.cmd [[
+  -- 		--   hi Normal guibg=NONE ctermbg=NONE
+  -- 		--   hi NormalNC guibg=NONE ctermbg=NONE
+  -- 		--   hi NormalFloat guibg=NONE ctermbg=NONE
+  -- 		--   hi SignColumn guibg=NONE ctermbg=NONE
+  -- 		-- ]]
+  -- 	end
+  -- },
+  -- {
+  -- 	"tiagovla/tokyodark.nvim",
+  -- 	opts = {
+  -- 		-- custom options here
+  -- 	},
+  -- 	config = function(_, opts)
+  -- 		require("tokyodark").setup(opts) -- calling setup is optional
+  -- 		vim.cmd [[colorscheme tokyodark]]
+  -- 	end,
+  -- },
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('lualine').setup()
+    end
+  },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {}
+  },
+  { "nvim-lua/plenary.nvim" },
+  {
+    -- Fuzzy finder for files, buffers, and more
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "plenary.nvim" }
+  },
+  {
+    -- Closes some gaps that exist between mason.nvim and nvim-dap
+    "jay-babu/mason-nvim-dap.nvim",
+    dependencies = { "williamboman/mason.nvim", "mfussenegger/nvim-dap" }
+  },
+  { 'tpope/vim-fugitive' }, -- Git plugin
+  { "mbbill/undotree" },    -- Inspect undo history of files
+  { "BurntSushi/ripgrep" }, -- Used by telescope
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    opts = {},
+  },
+  {
+    'MagicDuck/grug-far.nvim',
+    -- Note (lazy loading): grug-far.lua defers all it's requires so it's lazy by default
+    -- additional lazy config to defer loading is not really needed...
+    config = function()
+      -- optional setup call to override plugin options
+      -- alternatively you can set options with vim.g.grug_far = { ... }
+      require('grug-far').setup({
+        -- options, see Configuration section below
+        -- there are no required options atm
+      });
+    end
+  },
+  {
+    "hedyhli/outline.nvim",
+    lazy = true,
+    cmd = { "Outline", "OutlineOpen" },
+    opts = {
+      outline_window = {
+        -- open as right sidebar
+        focus_on_open = false,
+        split_command = "rightbelow vsplit",
+        show_numbers = true, -- real line numbers
+        show_relative_numbers = true,
+        width = 25,
+      },
+      outline_items = {
+        show_symbol_details = true,
+        show_symbol_lineno = true, -- show lineno in the outline
+        flatten_hierachy = true,
+      },
+      auto_preview = false, -- optional: prevents jumping while browsing
+      symbol_folding = {
+        autofold_depth = false,
+      },
+      preview_window = {
+        auto_preview = true,
+        open_hover_on_preview = true,
+      }
+    },
+  }
 }
