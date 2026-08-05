420f2b2b03822efc026546fa943a17c28abc9617
Andreas Hansson <andhan.kungen@gmail.com>
2026-08-05 16:20:04 +0200

    Replace lazy with vimpack (nvim's built in package manager)

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,29 +1,24 @@
-return {
-  "mfussenegger/nvim-dap",
-  config = function()
-    local dap = require("dap")
+local dap = require("dap")
 
-    dap.adapters.cppdbg = {
-      type = "executable",
-      command = "gdb",
-      args = {
-        "--interpreter=dap",
-      },
-    }
-
-    dap.configurations.cpp = {
-      {
-        name = "Launch",
-        type = "cppdbg",
-        request = "launch",
-        program = function()
-          return vim.fn.input("Executable: ")
-        end,
-        cwd = "${workspaceFolder}",
-        stopAtEntry = false,
-      },
-    }
-
-    dap.configurations.c = dap.configurations.cpp
-  end,
+dap.adapters.cppdbg = {
+  type = "executable",
+  command = "gdb",
+  args = {
+    "--interpreter=dap",
+  },
 }
+
+dap.configurations.cpp = {
+  {
+    name = "Launch",
+    type = "cppdbg",
+    request = "launch",
+    program = function()
+      return vim.fn.input("Executable: ")
+    end,
+    cwd = "${workspaceFolder}",
+    stopAtEntry = false,
+  },
+}
+
+dap.configurations.c = dap.configurations.cpp
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,18 +1,23 @@
-return {
-  {
-    'nvim-treesitter/nvim-treesitter',
-    lazy = false,
-    build = ':TSUpdate',
-    config = function()
-      local ts = require('nvim-treesitter')
-      ts.setup {
-        -- Directory to install parsers and queries to (prepended to `runtimepath` to have priority)
-        install_dir = vim.fn.stdpath('data') .. '/site'
-      }
-      ts.install({ "python", "qmljs", "javascript", "cpp", "cmake",
-        "gdscript", "c", "lua", "vim", "vimdoc",
-        "query", "markdown", "markdown_inline", "rust",
-        "gdshader", }):wait(300000) -- wait max. 5 minutes
-    end,
-  }
-}
+local ts = require("nvim-treesitter")
+
+ts.setup({
+  install_dir = vim.fn.stdpath("data") .. "/site",
+})
+
+ts.install({
+  "python",
+  "qmljs",
+  "javascript",
+  "cpp",
+  "cmake",
+  "gdscript",
+  "c",
+  "lua",
+  "vim",
+  "vimdoc",
+  "query",
+  "markdown",
+  "markdown_inline",
+  "rust",
+  "gdshader",
+}):wait(300000)
