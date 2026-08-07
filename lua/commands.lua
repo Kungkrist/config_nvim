8589912556741889dc360090c6fba3a1018bc50e
Andreas Hansson <andhan.kungen@gmail.com>
2026-08-07 12:09:32 +0200

    Add a SaveAll command that includes unexisting directories

--- lua/commands.lua
+++ lua/commands.lua
@@ -1,2 +1,6 @@
 -- Make Panel into an interactable buffer when focused (e.g., when doing :G status)
 require('vim._core.ui2').enable({})
+
+vim.api.nvim_create_user_command("SaveAll", function()
+  vim.cmd("wall ++p")
+end, {})
